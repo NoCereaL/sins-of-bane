9a53e25f8eb322f2ab55016fe11fbdb950c5664a
NoCereaL <therealskm@hotmail.com>
2021-06-17 20:40:08 +0100

    Update PickUpController.cs

--- Sins of Bane -Git/Assets/Photon/PhotonUnityNetworking/Resources/PickUpController.cs
+++ Sins of Bane -Git/Assets/Photon/PhotonUnityNetworking/Resources/PickUpController.cs
@@ -54,6 +54,8 @@ public class PickUpController : MonoBehaviourPun
             this.photonView.RPC("Send", RpcTarget.All);
         }
 
+
+
         JoinController2();
         ExecuteTheUpdate();
 
@@ -119,7 +121,7 @@ public class PickUpController : MonoBehaviourPun
 
             distanceToPlayer = player.position - transform.position;
 
-            if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull && PhotonNetwork.LocalPlayer.ActorNumber == 2)
+            if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull)
             {
                 Debug.Log(PhotonNetwork.LocalPlayer.UserId);
                 PickUp();
