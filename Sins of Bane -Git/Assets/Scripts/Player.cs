aca6e96732b62b03a9b0c97e25d0411a22eb5f82
NoCereaL <therealskm@hotmail.com>
2021-06-23 17:58:00 +0100

    Update WeaponHolder and Player

--- Sins of Bane -Git/Assets/Scripts/Player.cs
+++ Sins of Bane -Git/Assets/Scripts/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviourPun
     public HealthBarScript healthBar;
     public float RespawnX, RespawnY, RespawnZ;
 
-    public GameObject player;
+    public static GameObject player;
     // Start is called before the first frame update
     void Start()
     {
