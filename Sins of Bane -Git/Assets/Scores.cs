9bdef5cb71acca2c1754e78b419222f960ec95b5
NoCereaL <therealskm@hotmail.com>
2021-07-12 09:16:46 +0100

    Update Scores.cs

--- Sins of Bane -Git/Assets/Scores.cs
+++ Sins of Bane -Git/Assets/Scores.cs
@@ -26,6 +26,7 @@ public class Scores : MonoBehaviourPun
         photonView.RPC("GetDeaths", RpcTarget.AllBuffered);
     }
 
+    [PunRPC]
     void GetDeaths()
     {
         TeamOneScore = GameObject.Find("player(Clone)").GetComponent<PlayerInfo>().DeathCount;
