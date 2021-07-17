df3a7cdce4dd5689b06b9a608ac5b2ffc5be6db5
NoCereaL <therealskm@hotmail.com>
2021-07-17 11:57:31 +0100

    Update RoomListItem.cs

--- Sins of Bane -Git/Assets/Scripts/OnlineMenu/RoomListItem.cs
+++ Sins of Bane -Git/Assets/Scripts/OnlineMenu/RoomListItem.cs
@@ -13,7 +13,7 @@ public class RoomListItem : MonoBehaviour
 	public void SetUp(RoomInfo _info)
 	{
 		info = _info;
-		text.text = _info.Name + _info.PlayerCount;
+		text.text = _info.Name +"  -  "+ _info.PlayerCount +"/8";
 	}
 
 	public void OnClick()
