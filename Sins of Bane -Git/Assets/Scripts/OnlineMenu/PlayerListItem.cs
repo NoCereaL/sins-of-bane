d0cd496f812054e858ec6e66d5b391b93ce1eeeb
NoCereaL <therealskm@hotmail.com>
2021-07-25 03:21:55 +0100

    UPdate

--- Sins of Bane -Git/Assets/Scripts/OnlineMenu/PlayerListItem.cs
+++ Sins of Bane -Git/Assets/Scripts/OnlineMenu/PlayerListItem.cs
@@ -9,14 +9,12 @@ using UnityEngine.UI;
 public class PlayerListItem : MonoBehaviourPunCallbacks
 {
 	[SerializeField] TMP_Text text;
-	[SerializeField] Text text2;
 	Player player;
 
 	public void SetUp(Player _player)
 	{
 		player = _player;
 		text.text = _player.NickName;
-		text2.text = _player.NickName;
 	}
 
 	public override void OnPlayerLeftRoom(Player otherPlayer)
